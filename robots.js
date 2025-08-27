bfcd451c7267a57f2191a5470ff6212769be49ed
NMXLnm <3325403002@qq.com>
2025-08-27 00:58:49 -0700

    update robot avatars

--- robots.js
+++ robots.js
@@ -1,14 +1,14 @@
 // robots.js
 window.robots = [
-  { name: "GeitHub",    mood: "开心",   status: "在线", battery: 80, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54b683c0.jpg" },
-  { name: "Og yut",     mood: "不开心", status: "离线", battery: 50, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f860741.jpg" },
+  { name: "GeitHub",    mood: "开心",   status: "在线", battery: 80, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54cb0059.jpg" },
+  { name: "Og yut",     mood: "不开心", status: "离线", battery: 50, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f884ba7.jpg" },
   { name: "Yutpm",      mood: "心动",   status: "未知", battery: 65, avatar: "https://youke1.picui.cn/s1/2025/08/26/68ad90133db41.jpg" }, // 你没给，保持默认
-  { name: "Kickvo",     mood: "失望",   status: "在线", battery: 92, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f793ee7.jpg" },
-  { name: "NekoogramX", mood: "开心",   status: "离线", battery: 44, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f82c3a6.jpg" },
-  { name: "shizuGo",    mood: "不开心", status: "未知", battery: 70, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f884ba7.jpg" },
+  { name: "Kickvo",     mood: "失望",   status: "在线", battery: 92, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f89edfa.jpg" },
+  { name: "NekoogramX", mood: "开心",   status: "离线", battery: 44, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f793ee7.jpg" },
+  { name: "shizuGo",    mood: "不开心", status: "未知", battery: 70, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb5f82c3a6.jpg" },
   { name: "Shizuku",    mood: "心动",   status: "在线", battery: 38, avatar: "https://youke1.picui.cn/s1/2025/08/26/68ad90133db41.jpg" }, // 保持不变
   { name: "Katenell",   mood: "失望",   status: "离线", battery: 77, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54d2aa01.jpg" },
-  { name: "HUNK",       mood: "开心",   status: "在线", battery: 60, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54cb0059.jpg" },
+  { name: "HUNK",       mood: "开心",   status: "在线", battery: 60, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54ccb0f9.jpg" },
   { name: "GvitHub",    mood: "不开心", status: "未知", battery: 85, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb54cc7306.jpg" },
   { name: "埃安",        mood: "心动",   status: "在线", battery: 48, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb51eca7ae.jpg" },
   { name: "宝格丽乔",    mood: "失望",   status: "离线", battery: 93, avatar: "https://youke1.picui.cn/s1/2025/08/27/68aeb4872b611.jpg" },
